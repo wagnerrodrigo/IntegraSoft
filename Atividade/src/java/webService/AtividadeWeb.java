eb32e4873f447012bc9fadbe5ca103b4d26ac90d
wagner rodrigo da silva <wagnerrodrigo.pan@gmail.com>
2019-09-10 22:05:24 -0300

    atividade

---
+++ Atividade/src/java/webService/AtividadeWeb.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webService;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author alunoces
+ */
+@WebService(serviceName = "AtividadeWeb")
+@Stateless()
+public class AtividadeWeb {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+
+    /**
+     * Operação de Web service
+     */
+    @WebMethod(operationName = "opera\u00e7\u00e3o")
+    public Integer operação(@WebParam(name = "Valor1") int Valor1) {
+        //TODO write your implementation code here:
+        return null;
+    }
+     @WebMethod(operationName = "Valor_intervalo_entre 1 a 10")
+    public int somar(@WebParam(name = "numero1") int numero1,  @WebParam(name = "numero2") String txt) {
+     
+        if(numero1 <= 10){
+         return numero1;
+        }
+        return numero1 ;
+    }
+
+    /**
+     * Operação de Web service
+     */
+    @WebMethod(operationName = "comparar")
+    public Integer comparar(@WebParam(name = "numero1") int numero1, @WebParam(name = "numero2") int numero2) {
+        //TODO write your implementation code here:
+        String resp;
+        
+        if(numero1 == numero2)
+             System.out.println(" O Numero "+numero1+ "é iguial a " + numero2);
+        System.out.println(" O Numero "+numero1+ "não iguial a " + numero2);
+       return ;
+    }
+    
+}
